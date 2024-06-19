d0ede904d9e4e67491f74a192dbf305fc56e534c
Rodrigo Quisbert <seintecarica@gmail.com>
2024-06-19 01:15:08 -0400

    Prueba commit

--- resources/views/caja/form.blade.php
+++ resources/views/caja/form.blade.php
@@ -11,7 +11,7 @@
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
                         <li class="breadcrumb-item"><a href="{{ route('home') }}">Inicio</a></li>
-                        <li class="breadcrumb-item active">Caja</li>
+                        <li class="breadcrumb-item active">Cajas</li>
                     </ol>
                 </div><!-- /.col -->
             </div><!-- /.row -->
